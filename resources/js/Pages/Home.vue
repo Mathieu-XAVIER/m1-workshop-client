a6c951001442da22a88a0f913cbdbbcee0d9572c
ClementRollin <clemrollin01@gmail.com>
2025-06-30 13:59:50 +0200

    Create Register Page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-container">
+    <h1>Bienvenue !</h1>
+    <p>Vous êtes bien connecté à votre espace.</p>
+  </div>
+</template>
+
+<style scoped>
+.home-container {
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 2rem;
+  text-align: center;
+  font-family: sans-serif;
+}
+</style> 
