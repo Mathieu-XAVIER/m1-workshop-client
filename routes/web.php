8201c13ca9588d50935a79ad7f02005d8908c0d7
Alexandre Bernardini <bernardini.alexandre2003@gmail.com>
2025-06-30 14:26:09 +0200

    Changement de route du a un conflic de pull request

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
 <?php
-
+use App\Http\Controllers\Auth\RegistrationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Page d'accueil avec les catégories
 Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('welcome');
+
+Route::get('/login', function () {
+    return Inertia::render('Login');
+})->name('login');
+
+Route::post('/api/register', [RegistrationController::class, 'store'])->name('register');
+Route::post('/api/login', function () {
+    // Logique de connexion à implémenter par l'équipe back-end
+})->name('login.store');
+
+Route::get('/home', function () {
     return Inertia::render('Home');
-});
+})->name('home');
